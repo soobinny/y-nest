63b0f24453ad111ee31859bca4109430a0d1ad3d
soobinny <sbbin22@gmail.com>
2025-09-24 22:12:50 +0900

    🎉 add: 초기 세팅

---
+++ src/main/java/com/example/capstonedesign/CapstoneDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstonedesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneDesignApplication.class, args);
+    }
+
+}
