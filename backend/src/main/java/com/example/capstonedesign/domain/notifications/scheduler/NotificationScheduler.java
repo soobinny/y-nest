99fe9e669d145c79b98cc799f66162ab6b6a2678
soobinny <sbbin22@gmail.com>
2025-11-21 12:20:14 +0900

    💄style: 챗봇 background색상 변경

--- backend/src/main/java/com/example/capstonedesign/domain/notifications/scheduler/NotificationScheduler.java
+++ backend/src/main/java/com/example/capstonedesign/domain/notifications/scheduler/NotificationScheduler.java
@@ -20,7 +20,7 @@ public class NotificationScheduler {
 
     /** 매일 오전 6시 실행 */
 //    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")  // 테스트용 (1분마다)
-    @Scheduled(cron = "0 0 6 * * *", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
     public void run() {
         log.info("🕕 마감 임박 공고 알림 스케줄러 실행");
         service.sendDailyDigest();
